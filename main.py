40d1f6dda18babafbb8a26fb67350f4d9f154c3c
Lucas Mendes <lucasm.amorsaude@gmail.com>
2025-04-16 09:09:29 -0300

    Teste geral 1

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-import tkinter as tk
-from gui.main_window import MainWindow
-from gui.settings_window import SettingsWindow
-from services.webdental import gerar_relatorio
-from services.whatsapp import enviar_planilha_para_slack
+# main.py
 
+from services.webdental import gerar_relatorio
+from services.slack import enviar_planilha_para_slack
 
 def main():
-    root = tk.Tk()
+    # Passa as datas ou deixa que a função gerar_relatorio defina automaticamente
+    resultado_relatorio = gerar_relatorio()
 
-    def executar_envio(data_inicio, data_fim):
-        print(f"Enviando mensagens de {data_inicio} até {data_fim}...")
-        enviar_planilha_para_slack()  # Agora efetivamente dispara o envio
+    if resultado_relatorio["sucesso"]:
+        print(resultado_relatorio["mensagem"])
 
-    def abrir_configuracoes():
-        SettingsWindow(root)
-
-    app = MainWindow(
-        root,
-        on_execute_callback=executar_envio,
-        on_open_settings_callback=abrir_configuracoes
-    )
-    app.pack(fill="both", expand=True)
-    root.mainloop()
-
+        # Chama a função para enviar a planilha para o Slack
+        caminho_planilha = "data/relatorio_efetivados.xlsx"
+        enviar_planilha_para_slack(caminho_planilha)
+    else:
+        print(f"Erro ao gerar o relatório: {resultado_relatorio['mensagem']}")
 
 if __name__ == "__main__":
     main()
